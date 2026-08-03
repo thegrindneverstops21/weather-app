4cfb3411221ddd07f07a4c39c6681277174cf34c
thegrindneverstops21 <Learner@lcx.co.za>
2026-08-03 11:30:08 +0200

    added comments to the Weather icon component

--- src/components/WeatherIcon.tsx
+++ src/components/WeatherIcon.tsx
@@ -3,16 +3,19 @@ import {
 } from 'lucide-react';
 import type { LucideIcon } from 'lucide-react';
 
+/* table for weather icons */
 const ICON_MAP: Record<string, LucideIcon> = {
     Sun, Moon, CloudSun, CloudMoon, Cloud, CloudFog, CloudDrizzle, CloudRain, CloudRainWind, CloudSnow, CloudLightning,
 };
 
+/*interface that defines the shape of the Icon properties */
 interface WeatherIconProps {
     icon: string;
     size?: number;
     className?: string;
 }
 
+/* destructure props, looks for item in the table, displays help circle if no items are found */
 export default function WeatherIcon({ icon, size = 24, className }: WeatherIconProps) {
     const Icon = ICON_MAP[icon] || HelpCircle;
     return <Icon size={size} className={className} />;
