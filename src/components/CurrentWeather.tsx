7412bc6a4d88ca88eba9fb8603bdaf4b0fdc5149
thegrindneverstops21 <Learner@lcx.co.za>
2026-08-03 15:36:02 +0200

    final push for the work day: just added a few comments

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -5,23 +5,26 @@ import WeatherIcon from './WeatherIcon';
 import { MapPin } from 'lucide-react';
 import './styles/CurrentWeather.css';
 
+/* interface that represents the location, weather and unit of measurement */
 interface CurrentWeatherProps {
     location: Location;
     weather: CurrentWeatherType;
     units: UnitSettings;
 }
 
+/* a function component*/
 export default function CurrentWeather({ location, weather, units}: CurrentWeatherProps ){
     const info = getWeatherInfo(weather.weatherCode, weather.isDay);
     const temp = convertTemperature(weather.temperature, units.temperature);
 
+    /* a date object that formats the date to localized date */
     const formattedDate = new Date(weather.time).toLocaleDateString('en-ZA', {
         day: 'numeric',
         month: 'long',
         year: 'numeric',
         weekday: 'long',
     });
-
+    /* date object that formats the time to localized time*/
     const formattedTime = new Date(weather.time).toLocaleDateString('en-ZA', {
         hour: 'numeric',
         minute: '2-digit',
